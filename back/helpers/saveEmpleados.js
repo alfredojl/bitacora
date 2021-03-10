7a1e3d1d5fc89ff2da332d4f1574c4165cc0d4f2
alfredojl <alfredo.jl323@gmail.com>
2021-03-10 15:13:41 -0600

    Fotos

--- back/helpers/saveEmpleados.js
+++ back/helpers/saveEmpleados.js
@@ -16,42 +16,57 @@ mongoose.connect(config.urlDB, {
 });
 
 const saveEmpleados = async() => {
-    let workbook = xlsx.readFile('./empleadosDSH.xlsx');
-    let heads = workbook.SheetNames;
-    let errors = [],
-        empleados = [];
-    for ([index, el] of xlsx.utils.sheet_to_json(workbook.Sheets[heads[0]]).entries()) {
-        // console.log(index, el.nombre);
-        let photo = path.resolve(os.homedir() + '/employeesPhotos/' + el.nombre.split(' ')[0] + '-' +
-            el.telefono + '.png');
-        await Empleado.create({
-            name: el.nombreEmbozado,
-            phone: el.telefono,
-            email: el.correo,
-            photo
-        }, (err, empleadoDB) => {
-            if (err) errors.push(err)
-            empleados.push(empleadoDB);
+    Empleado.find({}, (err, empleadosDB) => {
+            if (err) throw new Error(err);
+            let count = 0;
+            empleadosDB.forEach(el => {
+                let photo = el.photo.split('.');
+                photo[1] = 'jpg';
+                photo = photo.join('.');
+                el.photo = photo;
+                Empleado.updateMany({ phone: el.phone }, el, (err, empleadoDB) => {
+                    if (err) throw new Error(err);
+                    count++;
+                    console.log('Hecho ', count);
+                })
+            })
         })
-    }
-    // xlsx.utils.sheet_to_json(workbook.Sheets[heads[0]]).forEach(async(el, index) => {
-    //     let photo = path.resolve(os.homedir() + '/employeesPhotos/' + el.nombre.split(' ')[0] + '-' +
-    //         el.telefono + '.png');
-    //     await Empleado.create({
-    //         name: el.nombre,
-    //         phone: el.telefono,
-    //         email: el.correo,
-    //         photo
-    //     }, (err, empleadoDB) => {
-    //         if (err) errors.push(err)
-    //         empleados.push(empleadoDB);
-    //     })
-    // })
-    if (errors.length > 0)
-        console.log('Hubo errores:\n', errors);
-    else
-        console.log('Sin errores');
-    console.log(empleados);
+        // let workbook = xlsx.readFile('./empleadosDSH.xlsx');
+        // let heads = workbook.SheetNames;
+        // let errors = [],
+        //     empleados = [];
+        // for ([index, el] of xlsx.utils.sheet_to_json(workbook.Sheets[heads[0]]).entries()) {
+        //     // console.log(index, el.nombre);
+        //     let photo = path.resolve(os.homedir() + '/employeesPhotos/' + el.nombre.split(' ')[0] + '-' +
+        //         el.telefono + '.png');
+        //     await Empleado.create({
+        //         name: el.nombreEmbozado,
+        //         phone: el.telefono,
+        //         email: el.correo,
+        //         photo
+        //     }, (err, empleadoDB) => {
+        //         if (err) errors.push(err)
+        //         empleados.push(empleadoDB);
+        //     })
+        // }
+        // xlsx.utils.sheet_to_json(workbook.Sheets[heads[0]]).forEach(async(el, index) => {
+        //     let photo = path.resolve(os.homedir() + '/employeesPhotos/' + el.nombre.split(' ')[0] + '-' +
+        //         el.telefono + '.png');
+        //     await Empleado.create({
+        //         name: el.nombre,
+        //         phone: el.telefono,
+        //         email: el.correo,
+        //         photo
+        //     }, (err, empleadoDB) => {
+        //         if (err) errors.push(err)
+        //         empleados.push(empleadoDB);
+        //     })
+        // })
+        // if (errors.length > 0)
+        //     console.log('Hubo errores:\n', errors);
+        // else
+        //     console.log('Sin errores');
+        // console.log(empleados);
 }
 
 saveEmpleados();
